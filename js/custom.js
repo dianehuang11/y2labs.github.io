3a3a8d62cd47fa5afad1ee4cd4bd1c6fd5b11726
dianehuang <diane.huangg@yahoo.com>
2016-12-24 23:10:01 -0500

    sushi!

--- js/custom.js
+++ js/custom.js
@@ -1,22 +1,62 @@
 (function() {
   'use strict';
 
+  function findPos(obj) {
+    var curtop = 0;
+    if (obj.offsetParent) {
+        do {
+            curtop += obj.offsetTop;
+        } while (obj = obj.offsetParent);
+    return curtop;
+    }
+  }
+  
+  document.getElementById('clickList').addEventListener('click', function(event) {
+    if (event.target.innerHTML) {
+      var nextTarget = document.getElementById(event.target.innerHTML.toLowerCase().replace(' ', '_'));
+      if (nextTarget) {
+        console.log(nextTarget.clientHeight);
+        window.scroll({
+          top: findPos(nextTarget) - nextTarget.clientHeight * 3, 
+          left: 0, 
+          behavior: 'smooth' 
+        });
+      }
+    }
+  });
+  
   var section = document.querySelectorAll(".scrollPast");
   var sections = {};
-  var i = 0;
+
+  var volunteerNavigation = document.querySelector('.volunteerPage__navigation');
+  var volunteerScrollPast = document.getElementById('volunteerScrollPast');
 
   Array.from(section).forEach(function(e){
     sections[e.id] = e.offsetTop;
   });
 
-  window.onscroll = function() {
-    var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
-    for (i in sections) {
+
+  var mainNavHidden = function (scrollPosition) {
+    for (var i in sections) {
       if (sections[i] <= scrollPosition) {
         document.querySelector('header').setAttribute('class', 'notTransparent');
       } else {
         document.querySelector('header').setAttribute('class', 'transparent');
       }
     }
+  }
+
+  var volunteerNavHidden = function (scrollPosition) {
+    if (volunteerScrollPast.offsetTop <= scrollPosition) {
+      volunteerNavigation.classList.add('active');
+    } else {
+      volunteerNavigation.classList.remove('active');
+    }
+  }
+
+  window.onscroll = function() {
+    var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+    mainNavHidden(scrollPosition);
+    volunteerNavHidden(scrollPosition);
   };
 })();
